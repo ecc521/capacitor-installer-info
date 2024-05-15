be760f71f87004cb76e76c0dee76ae2b7c7f65d5
Tucker Willenborg <tjw47@duke.edu>
2024-05-15 13:38:00 -0400

    Amend typescript types to make details optional to typescript

--- src/definitions.ts
+++ src/definitions.ts
@@ -12,8 +12,8 @@ export interface InstallerInfoPluginPlugin {
    * When "" (empty string) is returned, the value was null from the PackageManager API.
    * When no value is returned for a key, the value could not be calculated given the Android API level or for other reasons.
    *
-   * @param details {packageName: string} | undefined. If packageName undefined or no object passed, the package name of the current app is used.
+   * @param details {packageName: string}. If packageName undefined or no object passed, the package name of the current app is used.
    * @since 6.0.0
    */
-  getInstallSourceInfo(details: Record<string, string> | undefined): Promise<Record<string, string>>;
+  getInstallSourceInfo(details?: Record<string, string>): Promise<Record<string, string>>;
 }
